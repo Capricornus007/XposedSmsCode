b18c439912efd11d8a348460f4b331618a99df0e
magisk317 <code@mail.usdt.edu.kg>
2026-08-24 01:46:02 +0800

    chore: bump entitlement-android to 0.1.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -126,7 +126,7 @@ tasks.named("preBuild") {
 }
 
 dependencies {
-    implementation("com.magisk317.mobile:entitlement-android:0.1.11")
+    implementation("com.magisk317.mobile:entitlement-android:0.1.12")
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation(project(":core"))
     implementation(project(":hook"))
